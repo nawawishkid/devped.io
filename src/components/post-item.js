8e77c3dd5544f2b60a15c1326868978867504056
Nawawish Samerpark <hydrokidz@gmail.com>
2019-12-31 00:40:39 +0700

    update ui: display language of post

--- src/components/post-item.js
+++ src/components/post-item.js
@@ -1,18 +1,27 @@
 import React from "react"
 import Link from "./link"
+import { useLocale } from "../contexts/locale"
 
-const PostItem = props => (
-  <div>
-    <Link to={props.slug}>
-      <h3>{props.title}</h3>
-    </Link>
-    <small>{props.updatedAt}</small>
-    {/* <p>
+const PostItem = props => {
+  const { translate } = useLocale()
+
+  return (
+    <div>
+      <Link to={props.slug}>
+        <h3>{props.title}</h3>
+      </Link>
+      <small>{props.updatedAt}</small>
+      <br />
+      <small>
+        {translate(`language`, `post-item`, `component`)}: {props.locale}
+      </small>
+      {/* <p>
       {Array.isArray(props.tree.children)
         ? `${props.tree.children.length} lessons`
         : null}
     </p> */}
-  </div>
-)
+    </div>
+  )
+}
 
 export default PostItem
