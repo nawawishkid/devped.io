a420b554907f307d85a01f292084f97f287a81df
Nawawish Samerpark <hydrokidz@gmail.com>
2019-12-31 19:10:12 +0700

    add 'locale' prop allowing custom locale in url

--- src/components/link.js
+++ src/components/link.js
@@ -2,9 +2,9 @@ import React from "react"
 import { Link as GLink } from "gatsby"
 import { useLocale } from "../contexts/locale"
 
-const Link = ({ to, ...props }) => {
+const Link = ({ to, locale: customLocale = null, ...props }) => {
   const { locale } = useLocale()
-  const newUrl = `/` + locale + to
+  const newUrl = `/` + (customLocale || locale) + to
   console.log(`locale: `, locale)
   console.log(`newUrl: `, newUrl)
 
